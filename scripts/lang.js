35caa107e5137e50ba415aaf75e5ee10ce84e26b
Judson Gabriel <165574074+JudGB@users.noreply.github.com>
2026-05-20 14:11:51 -0400

    Version 1.0 myport
    primary version of my portifolio, welcome :)

---
+++ scripts/lang.js
@@ -0,0 +1,31 @@
+const translations = {
+    'Pt-Br': {
+        'Pt-Br': 'En',
+        'Home': 'Home',
+        'Contact': 'Fale Comigo',
+        'about': 'Sobre Mim',
+        'portifolio': 'Portifólio',
+        'prog': 'Programador | GameDesign',
+        'idt01': 'Sobre Mim',
+        'idt02': 'Prazer, me chamo Judson Gabriel Ferreira dos Santos, sou entusiasta de C++ e Unity, amante de GameMaker e nas horas livre programo algumas coisas interessantes, sou programador atuando em apenas jogos indies e sou também um GameDesigner gosto muito de jogos, fascinado por programação e a lógica que envolve tudo isso, serei grande muito muito grande.',
+        'Titulo': 'Portifólio',
+        'idt03': 'Run! Doctor Need Help” foi um jogo desenvolvido para uma atividade escolar sobre tipos sanguíneos, onde meu grupo decidiu transformar o tema em um jogo digital educativo. No jogo, o jogador precisa desviar de obstáculos em um hospital precário enquanto coleta bolsas de sangue corretas para diferentes pacientes; caso o tipo sanguíneo seja escolhido incorretamente, o paciente morre e o jogo explica tecnicamente o motivo, com base em pesquisas realizadas pela equipe. O projeto foi inteiramente desenvolvido na engine GameMaker Studio 2, com programação, idealização e artes feitas por mim.',
+        'idt04': '"Arrows Running" é um jogo feito por mim em 2024 para uma competição (gamejam) que tinha com objetivo um jogo que fosse simples e fosse sobre "armas", como espadas, lanças e arcos, por isso a premissa do jogo, você é uma flecha que percorre o quanto conseguir o mapa e vai avançando, ficando sempre mais difícil, jogo feito no GameMaker em apenas 1 dia, toda arte, ideia e programação.',
+        'idt05': 'Fale Comigo'
+
+    },
+    'en': {
+        'Pt-Br': 'Pt-Br',
+        'Home': 'Home',
+        'Contact': 'Fale Comigo',
+        'about': 'Sobre Mim',
+        'portifolio': 'Portifólio',
+        'prog': 'Programador | GameDesign',
+        'idt01': 'Sobre Mim',
+        'idt02': 'Prazer, me chamo Judson Gabriel Ferreira dos Santos, sou entusiasta de C++ e Unity, amante de GameMaker e nas horas livre programo algumas coisas interessantes, sou programador atuando em apenas jogos indies e sou também um GameDesigner gosto muito de jogos, fascinado por programação e a lógica que envolve tudo isso, serei grande muito muito grande.',
+        'Titulo': 'Portifólio',
+        'idt03': 'Run! Doctor Need Help” foi um jogo desenvolvido para uma atividade escolar sobre tipos sanguíneos, onde meu grupo decidiu transformar o tema em um jogo digital educativo. No jogo, o jogador precisa desviar de obstáculos em um hospital precário enquanto coleta bolsas de sangue corretas para diferentes pacientes; caso o tipo sanguíneo seja escolhido incorretamente, o paciente morre e o jogo explica tecnicamente o motivo, com base em pesquisas realizadas pela equipe. O projeto foi inteiramente desenvolvido na engine GameMaker Studio 2, com programação, idealização e artes feitas por mim.',
+        'idt04': '"Arrows Running" é um jogo feito por mim em 2024 para uma competição (gamejam) que tinha com objetivo um jogo que fosse simples e fosse sobre "armas", como espadas, lanças e arcos, por isso a premissa do jogo, você é uma flecha que percorre o quanto conseguir o mapa e vai avançando, ficando sempre mais difícil, jogo feito no GameMaker em apenas 1 dia, toda arte, ideia e programação.',
+        'idt05': 'Fale Comigo'
+    }
+};
